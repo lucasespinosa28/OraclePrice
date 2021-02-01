bcd975433ca206618d76fa6fad90a665268480d6
lucas espinosa <lucasespinosa159@gmail.com>
2021-02-01 04:34:48 -0300

    fix name

--- PriceOracle.Core/Connect.cs
+++ PriceOracle.Core/Connect.cs
@@ -5,6 +5,6 @@ namespace PriceOracle.Core
 {
     public class Connect
     {
-        public static Web3 Web3 { get; set; } = new Web3("https://mainnet.infura.io/v3/");
+        public static Web3 Web3 { get; set; } = new Web3("https://mainnet.infura.io/v3/ee20058d36674d109fd4d0df4e5e1112");
     }
 }
